37c73e61ce17a64e5bd6f2ce1c118aced2ed38bd
yueht23 <yueht17@gmail.com>
2024-05-10 10:56:26 +0100

    modify setpy

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+long_description = (this_directory / "Readme.md").read_text()
+
 setup(
     name='PySurvMC',
-    version='0.1',
+    version='0.2',
     author='yueht17',
     author_email="yueht17@foxmail.com",
-    description='A Python package for Survival Analysis with Monte Carlo Simulation',
+    # description='A Python package for Survival Analysis with Monte Carlo Simulation',
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
     packages=find_packages(),
     install_requires=[
         "pymc>=5.14.0",
